048f6c4e51808d9104446ff897b55eaf4dc51108
sengxai <60667952+sengxai@users.noreply.github.com>
2022-04-30 14:43:19 -0500

    Update ThirdLevelGUI.java

--- src/thirdLevel/ThirdLevelGUI.java
+++ src/thirdLevel/ThirdLevelGUI.java
@@ -1228,8 +1228,8 @@ public class ThirdLevelGUI extends javax.swing.JFrame {
         String to = recipient;
 
         // Sender's credentials
-        String from = "root.3la@gmail.com";
-        String emailPassword = "Test@1234";
+        String from = "*****@gmail.com";
+        String emailPassword = "*****";
 
         // Assuming you are sending email from through gmails smtp
         String host = "smtp.gmail.com";
